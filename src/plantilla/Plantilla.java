f9cb2873cf3bfdce7e4d9bf95f77765c43bc9bda
MeritxellMontesinos <meritxell.montesinos95@gmail.com>
2022-05-10 13:45:11 +0200

    Primera versión

---
+++ src/plantilla/Plantilla.java
@@ -0,0 +1,45 @@
+package plantilla;
+
+public class Plantilla {
+
+	public static void main(String[] args) {
+		//p1();
+		p2();
+		//p3();
+		//p4();
+		//p5();
+		//p6();
+		//p7();
+		//p8();
+		//p9();
+		//p10();
+		//p11();
+		//p12();
+		//p13();
+		//p14();
+	}
+
+	public static void p1() {
+		Boolean x = true;
+		Boolean y = false;
+		System.out.println(x || y);
+		System.out.println(x && y);
+	}
+	public static void p2() {
+		int x=7;
+		System.out.println(x == 25);
+	}
+
+	public static void p3() {}
+	public static void p4() {}
+	public static void p5() {}
+	public static void p6() {}
+	public static void p7() {}
+	public static void p8() {}
+	public static void p9() {}
+	public static void p10() {}
+	public static void p11() {}
+	public static void p12() {}
+	public static void p13() {}
+	public static void p14() {}
+}
